91e5eb04174637b815a776d1edb5d2956f6709e6
Wallace Coleman <wallardcoleman@gmail.com>
2018-11-20 16:57:26 -0500

    added Activity for chess

---
+++ app/src/main/java/com/example/guanghuili/checkesandchess/Chess/Chess.java
@@ -0,0 +1,15 @@
+package com.example.guanghuili.checkesandchess.Chess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.guanghuili.checkesandchess.R;
+
+public class Chess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chess);
+    }
+}
